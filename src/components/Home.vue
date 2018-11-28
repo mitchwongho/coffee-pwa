53c7be308b2e2d5102fd035a85a0cd97ea00210c
Mitchell Wong Ho <mitchell.wongho@absa.co.za>
2018-11-28 15:10:47 +0200

    Added Views and updated to use Material Design Lite styling

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<!-- Template -->
+<template>
+    <ul class="list">
+    </ul>
+</template>
+<!-- Scripting-->
+<script>
+export default {}
+</script>
+<!-- Style -->
+<style scoped>
+
+</style>
+
